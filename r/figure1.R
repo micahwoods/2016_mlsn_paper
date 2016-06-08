821e5164940fb61c82303052450713de57c74bc2
micah <micah@asianturfgrass.com>
2016-06-08 16:36:10 +0300

    updated cex to 1.5 and changed from ppm to mg kg-1

--- r/figure1.R
+++ r/figure1.R
@@ -1,7 +1,7 @@
 # make histogram figure for the MLSN paper
 
 fiskHist <- function(zed, xLabInput) {
-myhist <- hist(zed, breaks = 50, plot = FALSE)
+myhist <- hist(zed, breaks = 50, plot = FALSE, cex = 1.3)
 
 multiplier <- myhist$counts / myhist$density
 mydensity <- density(zed, na.rm = TRUE)
@@ -12,7 +12,8 @@ mymean <- mean(zed, na.rm = TRUE)
 mysd <- sd(zed, na.rm = TRUE)
 normal <- dnorm(x = myx, mean = mymean, sd = mysd)
 
-hist(zed, breaks = 30, main = NULL, xlab = bquote(.(xLabInput)))
+hist(zed, breaks = 30, main = NULL, xlab = bquote(paste(.(xLabInput), ~kg^{-1}*")")), 
+     cex.lab = 1.5, cex.axis = 1.5, cex.main = 1.5, cex.sub = 1.5)
 lines(mydensity, col = "#7570b3", lwd = 2, main = NULL, lty = 1)
 lines(myx, normal * multiplier[1], col = "#d95f02", lwd = 2, main = NULL, lty = 2)
 
@@ -27,11 +28,11 @@ lines(myx, fisk.nums * multiplier[1], col = "#1b9e77", lwd = 2, main = NULL, lty
 
 par(mfrow=c(3,2))
 par(mar=c(5.1,4.1,4.1,2.1))
-fiskHist(potassium, "K (ppm)")
-fiskHist(phosphorus, "P (ppm)")
-fiskHist(calcium, "Ca (ppm)")
-fiskHist(magnesium, "Mg (ppm)")
-fiskHist(sulfur, "S (ppm)")
+fiskHist(potassium, "K (mg")
+fiskHist(phosphorus, "P (mg")
+fiskHist(calcium, "Ca (mg")
+fiskHist(magnesium, "Mg (mg")
+fiskHist(sulfur, "S (mg")
 
 plot(x = 0, y = 0, type='n', bty='n', xaxt='n', yaxt='n',
      xlab = "", ylab = "")
@@ -39,4 +40,5 @@ plot(x = 0, y = 0, type='n', bty='n', xaxt='n', yaxt='n',
 legend("center",
        c("density", "normal", "log-logistic"),
        col=c("#7570b3", "#d95f02", "#1b9e77"),
-       lwd = 2, lty = c(1, 2, 6), bty = "n")
+       lwd = 2, lty = c(1, 2, 6), bty = "n",
+       cex = 1.5)
