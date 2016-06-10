21e94aea97e64cd0e44fe557c55a1c31322c1095
micah <micah@asianturfgrass.com>
2016-06-10 17:21:17 +0300

    update y-axis scale of the overlying curves

--- r/figure1.R
+++ r/figure1.R
@@ -1,28 +1,30 @@
 # make histogram figure for the MLSN paper
 
 fiskHist <- function(zed, xLabInput) {
-myhist <- hist(zed, breaks = 50, plot = FALSE, cex = 1.3)
+  
+myhist <- hist(zed, breaks = 40, plot = FALSE)
 
 multiplier <- myhist$counts / myhist$density
 mydensity <- density(zed, na.rm = TRUE)
-mydensity$y <- mydensity$y * multiplier[1]
+# I reduce with a factor of 0.94 so that all the curves remain on plot
+mydensity$y <- mydensity$y * multiplier[1] * 0.94
 
-myx <- seq(min(zed, na.rm = TRUE), max(zed, na.rm = TRUE), length.out= 200)
+myx <- seq(min(zed, na.rm = TRUE), max(zed, na.rm = TRUE), length.out = 200)
 mymean <- mean(zed, na.rm = TRUE)
 mysd <- sd(zed, na.rm = TRUE)
 normal <- dnorm(x = myx, mean = mymean, sd = mysd)
 
-hist(zed, breaks = 30, main = NULL, xlab = bquote(paste(.(xLabInput), ~kg^{-1}*")")), 
+hist(zed, breaks = 40, main = NULL, xlab = bquote(paste(.(xLabInput), ~kg^{-1}*")")), 
      cex.lab = 1.5, cex.axis = 1.5, cex.main = 1.5, cex.sub = 1.5)
 lines(mydensity, col = "#7570b3", lwd = 2, main = NULL, lty = 1)
-lines(myx, normal * multiplier[1], col = "#d95f02", lwd = 2, main = NULL, lty = 2)
+lines(myx, normal * multiplier[1] * 0.94, col = "#d95f02", lwd = 2, main = NULL, lty = 2)
 
 fit.x <- vglm(zed ~ 1, fisk)
 z <- Coef(fit.x)
 fisk.nums <- dfisk(myx, z[1], z[2])
 
 
-lines(myx, fisk.nums * multiplier[1], col = "#1b9e77", lwd = 2, main = NULL, lty = 6)
+lines(myx, fisk.nums * multiplier[1] * 0.94, col = "#1b9e77", lwd = 2, main = NULL, lty = 6)
 
 }
 
