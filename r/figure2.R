77945459ea3733674c8624881db2f496eb7e2408
micah <micah@asianturfgrass.com>
2016-07-08 20:01:54 +0700

    trying colour and line changes to make better correspondence with figure 1

--- r/figure2.R
+++ r/figure2.R
@@ -7,6 +7,10 @@
 
 ecdfPlot <- function(zed, xLabInput) {
   
+# col2 to be for the data
+# trying to make clearer distinction between data and model
+  
+col2 <- adjustcolor("grey", alpha.f = 0.6)  
 fit.x <- vglm(zed ~ 1, fisk)
 z <- Coef(fit.x)
 
@@ -17,14 +21,14 @@ sim.data <- data.frame(y = rfisk(n = length(na.omit(zed)),
 # makes a plot
 
 plot(ecdf(zed), verticals=TRUE, do.p=FALSE,
-     col.h = "red",
-     col.v = "red", 
+     col.h = col2,
+     col.v = col2, 
      main = NULL, xlab = bquote(paste(.(xLabInput), ~kg^{-1}*")")),
      cex.lab = 1.5, cex.axis = 1.5, cex.main = 1.5, cex.sub = 1.5)
 
 lines(ecdf(sim.data$y), verticals = FALSE,
-      col.h = "grey",
-      col.v = "grey")
+      col.h = "#1b9e77",
+      col.v = "#1b9e77")
 
 }
 
@@ -67,10 +71,10 @@ ecdfPlot(sulfur, "S (mg")
 plot(x = 0, y = 0, type='n', bty='n', xaxt='n', yaxt='n',
      xlab = "", ylab = "")
 
-col <- c("#d95f02", "#1b9e77")
-col2 <- adjustcolor(col, alpha.f = 0.6)
+# col <- c("#d95f02", "#1b9e77")
+
 legend("center", c("data", "MLSN model"),
-       col = c("red", "grey"),
+       col = c(col2, "#1b9e77"),
        lty = 1,
        lwd = 1,
        bty = "n",
